e25490846b5bc504b650cb405ce54e4e860f1402
rajes95 <sakhamuru.r@husky.neu.edu>
2020-06-25 11:13:08 -0700

    typo fix, also ordered responses in descending order for Q8

--- PM3.sql
+++ PM3.sql
@@ -13,7 +13,8 @@ ORDER BY `Covid1`.`Date`;
 #8. What is the Case Fatality Rate of Covid-19 by State?
 SELECT `State`.`StateName`, (sum(`Covid1`.`CovidDeaths`) - ifnull(sum(`Covid2`.`CovidDeaths`), 0)) / (sum(`Covid1`.`CovidCases`) - ifnull(sum(`Covid2`.`CovidCases`), 0)) * 100 AS `CaseFatalityRate`
 FROM `CovidByDate` AS `Covid1`
-LEFT JOIN `CovidBYDate` AS `Covid2` ON  datediff(`Covid1`.`Date`, `Covid2`.`Date`) = 1 AND `Covid1`.`CountyFKey` = `Covid2`.`CountyFKey`
+LEFT JOIN `CovidByDate` AS `Covid2` ON  datediff(`Covid1`.`Date`, `Covid2`.`Date`) = 1 AND `Covid1`.`CountyFKey` = `Covid2`.`CountyFKey`
 JOIN `County` ON `Covid1`.`CountyFKey` = `County`.`CountyKey` 
 JOIN `State` ON `County`.`StateFKey` = `State`.`StateKey`
-GROUP BY `State`.`StateName`;
+GROUP BY `State`.`StateName`
+ORDER BY `CaseFatalityRate` DESC;
