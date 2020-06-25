5dfce4e083024c88bdba62ce84b4a005e78ae63b
AriFleischer <fleischer.a@husky.neu.edu>
2020-06-25 09:25:41 -0400

    Fixed wording

--- PM3.sql
+++ PM3.sql
@@ -10,8 +10,8 @@ WHERE `County`.`StateFKey` = 21
 GROUP BY `Covid1`.`Date`
 ORDER BY `Covid1`.`Date`;
 
-#8. Number of Deaths per Confirmed Covid-19 Case, Case Fatality Rate, by State
-SELECT `State`.`StateName`, (sum(`Covid1`.`CovidDeaths`) - ifnull(sum(`Covid2`.`CovidDeaths`), 0)) / (sum(`Covid1`.`CovidCases`) - ifnull(sum(`Covid2`.`CovidCases`), 0)) * 100 AS `DeathsPerCase`
+#8. What is the Case Fatality Rate of Covid-19 by State?
+SELECT `State`.`StateName`, (sum(`Covid1`.`CovidDeaths`) - ifnull(sum(`Covid2`.`CovidDeaths`), 0)) / (sum(`Covid1`.`CovidCases`) - ifnull(sum(`Covid2`.`CovidCases`), 0)) * 100 AS `CaseFatalityRate`
 FROM `CovidByDate` AS `Covid1`
 LEFT JOIN `CovidBYDate` AS `Covid2` ON  datediff(`Covid1`.`Date`, `Covid2`.`Date`) = 1 AND `Covid1`.`CountyFKey` = `Covid2`.`CountyFKey`
 JOIN `County` ON `Covid1`.`CountyFKey` = `County`.`CountyKey` 
