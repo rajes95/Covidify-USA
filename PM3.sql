2e42458789204b354e2139634ff00b94dbbced8c
rajes95 <sakhamuru.r@husky.neu.edu>
2020-06-25 11:30:13 -0700

    typo fix and slight question modification

--- PM3.sql
+++ PM3.sql
@@ -1,7 +1,7 @@
 USE `CovidifyUSA`;
 SET SQL_SAFE_UPDATES = 0;
 
-#1. How many new Covid cases has Massachussetts had per day over time
+#1. How many new Covid cases has Massachussetts had per day over time?
 SELECT `Covid1`.`Date`, sum(`Covid1`.`CovidCases`) - ifnull(sum(`Covid2`.`CovidCases`), 0) AS `NumOfNewCases`
 FROM `CovidByDate` AS `Covid1`
 LEFT JOIN `CovidByDate` AS `Covid2` ON  datediff(`Covid1`.`Date`, `Covid2`.`Date`) = 1 AND `Covid1`.`CountyFKey` = `Covid2`.`CountyFKey`
@@ -10,7 +10,7 @@ WHERE `County`.`StateFKey` = 21
 GROUP BY `Covid1`.`Date`
 ORDER BY `Covid1`.`Date`;
 
-#8. What is the Case Fatality Rate of Covid-19 by State?
+#8. What is the Case Fatality Rate of Covid-19 by State from worst to best?
 SELECT `State`.`StateName`, (sum(`Covid1`.`CovidDeaths`) - ifnull(sum(`Covid2`.`CovidDeaths`), 0)) / (sum(`Covid1`.`CovidCases`) - ifnull(sum(`Covid2`.`CovidCases`), 0)) * 100 AS `CaseFatalityRate`
 FROM `CovidByDate` AS `Covid1`
 LEFT JOIN `CovidByDate` AS `Covid2` ON  datediff(`Covid1`.`Date`, `Covid2`.`Date`) = 1 AND `Covid1`.`CountyFKey` = `Covid2`.`CountyFKey`
