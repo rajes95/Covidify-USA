c03a70f5527c3609797790be5daeec36bd3218ad
rajes95 <sakhamuru.r@husky.neu.edu>
2020-06-25 11:04:19 -0700

    slight typo fix that broke question 1

--- PM3.sql
+++ PM3.sql
@@ -4,7 +4,7 @@ SET SQL_SAFE_UPDATES = 0;
 #1. How many new Covid cases has Massachussetts had per day over time
 SELECT `Covid1`.`Date`, sum(`Covid1`.`CovidCases`) - ifnull(sum(`Covid2`.`CovidCases`), 0) `NumOfCases`
 FROM `CovidByDate` AS `Covid1`
-LEFT JOIN `CovidBYDate` AS `Covid2` ON  datediff(`Covid1`.`Date`, `Covid2`.`Date`) = 1 AND `Covid1`.`CountyFKey` = `Covid2`.`CountyFKey`
+LEFT JOIN `CovidByDate` AS `Covid2` ON  datediff(`Covid1`.`Date`, `Covid2`.`Date`) = 1 AND `Covid1`.`CountyFKey` = `Covid2`.`CountyFKey`
 JOIN `County` ON `Covid1`.`CountyFKey` = `County`.`CountyKey` 
 WHERE `County`.`StateFKey` = 21
 GROUP BY `Covid1`.`Date`
