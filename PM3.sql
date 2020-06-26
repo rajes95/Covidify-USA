447623427be33de47860bda8f1f914b94fcf7026
rajes95 <sakhamuru.r@husky.neu.edu>
2020-06-25 17:22:07 -0700

    finished half of Q2 for PM3

--- PM3.sql
+++ PM3.sql
@@ -10,7 +10,25 @@ WHERE `County`.`StateFKey` = 21
 GROUP BY `Covid1`.`Date`
 ORDER BY `Covid1`.`Date`;
 
-#8. What is the Case Fatality Rate of Covid-19 by State from worst to best?
+#2. Top 5 and Least 5 states- how did they vote?
+
+select StateKey, StateName, PresidentialElectionVotePercentages.`Year` as ElectionYear,
+        (Sum(TotalPopulation* DemocratsPercent)/(Sum(TotalPopulation* DemocratsPercent)+Sum(TotalPopulation* RepublicansPercent)+Sum(TotalPopulation* OtherPercent))) as DemPerc,
+        (Sum(TotalPopulation* RepublicansPercent)/(Sum(TotalPopulation* DemocratsPercent)+Sum(TotalPopulation* RepublicansPercent)+Sum(TotalPopulation* OtherPercent))) as RepPerc,
+        (Sum(TotalPopulation* OtherPercent)/(Sum(TotalPopulation* DemocratsPercent)+Sum(TotalPopulation* RepublicansPercent)+Sum(TotalPopulation* OtherPercent))) as OtherPerc
+from PresidentialElectionVotePercentages
+	inner join County on PresidentialElectionVotePercentages.CountyFKey=County.CountyKey
+    inner join State on StateFkey=StateKey
+    inner join Population on Population.CountyFKey=County.CountyKey
+where PresidentialElectionVotePercentages.`Year`=2016 and Population.`Year`=2016
+group by StateName
+-- For some reason missing alaska election data from dataset so this cleans that up
+having (DemPerc is not null and RepPerc is not null and OtherPerc is not null); 
+
+
+#3. COVID-19 deaths per day over the last 3 months stratified by political region (i.e. states with democratic governors vs states with republican governors?)
+
+#8. What is the Case Fatality Rate of Covid-19 by State in descending order?
 SELECT `State`.`StateName`, (sum(`Covid1`.`CovidDeaths`) - ifnull(sum(`Covid2`.`CovidDeaths`), 0)) / (sum(`Covid1`.`CovidCases`) - ifnull(sum(`Covid2`.`CovidCases`), 0)) * 100 AS `CaseFatalityRate`
 FROM `CovidByDate` AS `Covid1`
 LEFT JOIN `CovidByDate` AS `Covid2` ON  datediff(`Covid1`.`Date`, `Covid2`.`Date`) = 1 AND `Covid1`.`CountyFKey` = `Covid2`.`CountyFKey`
