d0a67165c375439a364ec0e49732a31c72b47d7f
rajes95 <sakhamuru.r@husky.neu.edu>
2020-06-25 20:36:06 -0700

    Completed Q3 PM03,mysql select query for New Covid Cases per day splitting the country by Governor political party

--- PM3.sql
+++ PM3.sql
@@ -1,17 +1,19 @@
 USE `CovidifyUSA`;
 SET SQL_SAFE_UPDATES = 0;
+# Rajesh Sakhamuru, Elise Jortberg, Ari Fleischer, Lily Bessette
+# REAL TEAM SIX -> COVIDIFY-USA -> PM03
 
 #1. How many new Covid cases has Massachussetts had per day over time?
-SELECT `Covid1`.`Date`, sum(`Covid1`.`CovidCases`) - ifnull(sum(`Covid2`.`CovidCases`), 0) AS `NumOfNewCases`
+SELECT County.StateFKey, `Covid1`.`Date`, sum(`Covid1`.`CovidCases` - ifnull((`Covid2`.`CovidCases`), 0)) AS `NumOfNewCases`
 FROM `CovidByDate` AS `Covid1`
 LEFT JOIN `CovidByDate` AS `Covid2` ON  datediff(`Covid1`.`Date`, `Covid2`.`Date`) = 1 AND `Covid1`.`CountyFKey` = `Covid2`.`CountyFKey`
-JOIN `County` ON `Covid1`.`CountyFKey` = `County`.`CountyKey` 
+JOIN `County` ON `Covid1`.`CountyFKey` = `County`.`CountyKey`  
 WHERE `County`.`StateFKey` = 21
-GROUP BY `Covid1`.`Date`
-ORDER BY `Covid1`.`Date`;
+GROUP BY `County`.StateFKey,`Covid1`.`Date`
+ORDER BY `County`.StateFKey,`Covid1`.`Date`;
 
 #2. How did States vote in 2016, sorted by Descending COVID19 Case Fatality Ratio?
--- TODO: We should move this question to after # 8 probably since we are reusing the case fatality ratio code.
+-- TODO: We should move this question to after #8 probably since I am reusing the case fatality ratio code here.
 select StateName, CaseFatalityRate, ElectionYear, TotalPopulation, DemPerc, RepPerc, OtherPerc
 from (
 		select StateKey, StateName, PresidentialElectionVotePercentages.`Year` as ElectionYear, Sum(TotalPopulation) as TotalPopulation,
@@ -37,7 +39,29 @@ from (
 	on stateVotes2016.StateKey=caseFatalities.StateKey
 order by `CaseFatalityRate` DESC;
 
-#3. COVID-19 deaths per day over the last 3 months stratified by political region (i.e. states with democratic governors vs states with republican governors?)
+#3. COVID-19 new cases per day over time stratified by Governor Political affiliation (i.e. states with democratic vs republican governors).
+select CovidDate, sum(newCasesInDemStates) as DemStatesNewCovidCases, sum(newCasesinRepStates) as RepStatesNewCovidCases
+from (
+	select  GovernorParty, CovidDate,
+		sum(CASE WHEN GovernorParty="Democratic" THEN `NumOfNewCases` else 0 END) as NewCasesInDemStates,
+		sum(CASE WHEN GovernorParty="Republican" THEN `NumOfNewCases` else 0 END) as NewCasesInRepStates
+	from 
+			(select StateKey, StateName, StateGovernor.`Year` as `Year`, Governor, GovernorParty
+			from State inner join StateGovernor on StateKey=StateFKey
+			where StateGovernor.`Year` = 2020) Govs
+		inner join
+			(SELECT County.StateFKey as StateKey, `Covid1`.`Date` as CovidDate, sum(`Covid1`.`CovidCases` - ifnull((`Covid2`.`CovidCases`), 0)) AS `NumOfNewCases`
+			FROM `CovidByDate` AS `Covid1`
+			LEFT JOIN `CovidByDate` AS `Covid2` ON  datediff(`Covid1`.`Date`, `Covid2`.`Date`) = 1 AND `Covid1`.`CountyFKey` = `Covid2`.`CountyFKey`
+			JOIN `County` ON `Covid1`.`CountyFKey` = `County`.`CountyKey`  
+			GROUP BY `County`.StateFKey,`Covid1`.`Date`
+			ORDER BY `County`.StateFKey,`Covid1`.`Date`) StateCovidDaily
+		on Govs.StateKey=StateCovidDaily.StateKey
+	where Govs.`Year` = year(CovidDate)
+	group by CovidDate, GovernorParty
+	order by GovernorParty, CovidDate) NewCasesPartySplit
+group by CovidDate
+order by CovidDate;
 
 #8. What is the Case Fatality Rate of Covid-19 by State in descending order?
 SELECT `State`.`StateName`, (sum(`Covid1`.`CovidDeaths`) - ifnull(sum(`Covid2`.`CovidDeaths`), 0)) / (sum(`Covid1`.`CovidCases`) - ifnull(sum(`Covid2`.`CovidCases`), 0)) * 100 AS `CaseFatalityRate`
