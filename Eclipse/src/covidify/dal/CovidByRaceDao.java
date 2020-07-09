ead2db5a96d41b78f40aa4b797b015480e51d805
elisejortberg <jortberg.e@husky.neu.edu>
2020-07-09 16:09:47 -0400

    Update CovidByRaceDao.java

--- Eclipse/src/covidify/dal/CovidByRaceDao.java
+++ Eclipse/src/covidify/dal/CovidByRaceDao.java
@@ -6,109 +6,69 @@
 package covidify.dal;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Types;
-import java.util.ArrayList;
-import java.util.List;
 
-import covidify.model.County;
-import covidify.model.MortalityRates;
-//TODO here onwards
+import covidify.model.CovidByRace;
+import covidify.model.State;
+
 public class CovidByRaceDao {
   protected ConnectionManager connectionManager;
-  private static RestaurantDao instance = null;
+  private static CovidByRaceDao instance = null;
 
-  protected RestaurantDao() {
+  protected CovidByRaceDao() {
     connectionManager = new ConnectionManager();
   }
-  public static RestaurantDao getInstance() {
-    if(instance == null) {
-      instance = new RestaurantDao();
+
+  public static CovidByRaceDao getInstance() {
+    if (instance == null) {
+      instance = new CovidByRaceDao();
     }
     return instance;
   }
 
-  public MortalityRates create(MortalityRates mortalityRates) throws SQLException {
-    String insertRestaurant = "INSERT INTO Restaurant(Name,Description,Menu,ListedHours,"
-            + "IsActive,Street1,Street2,City,State,ZipCode,Cuisine,CompanyKey) "
-            + "VALUES(?,?,?,?,?,?,?,?,?,?,?,?);";
+  public CovidByRace create(CovidByRace covidByRace) throws SQLException {
+    String insertCovidByRace = "INSERT INTO CovidByRace(StateFKey,Race,Postive,Negative,Death,Date) "
+            + "VALUES(?,?,?,?,?,?);";
     Connection connection = null;
     PreparedStatement insertStmt = null;
     ResultSet resultKey = null;
     try {
       connection = connectionManager.getConnection();
-      insertStmt = connection.prepareStatement(insertRestaurant, Statement.RETURN_GENERATED_KEYS);
-      insertStmt.setString(1, mortalityRates.getName());
-      if (mortalityRates.getDescription() == null) {
-        insertStmt.setNull(2, Types.LONGVARCHAR);
+      insertStmt = connection.prepareStatement(insertCovidByRace, Statement.RETURN_GENERATED_KEYS);
+      if (covidByRace.getPositive() == null) {
+        insertStmt.setNull(1, Types.VARCHAR);
       } else {
-        insertStmt.setString(2, mortalityRates.getDescription());
+        insertStmt.setInt(1, covidByRace.getPositive());
       }
-      if (mortalityRates.getMenu() == null) {
-        insertStmt.setNull(3, Types.LONGVARCHAR);
-      } else {
-        insertStmt.setString(3, mortalityRates.getMenu());
-      }
-      if (mortalityRates.getListedHours() == null) {
-        insertStmt.setNull(4, Types.LONGVARCHAR);
-      } else {
-        insertStmt.setString(4, mortalityRates.getListedHours());
-      }
-      if (mortalityRates.getIsActive() == null) {
-        insertStmt.setNull(5, Types.BOOLEAN);
-      } else {
-        insertStmt.setBoolean(5, mortalityRates.getIsActive());
-      }
-      if (mortalityRates.getStreet1() == null) {
-        insertStmt.setNull(6, Types.VARCHAR);
-      } else {
-        insertStmt.setString(6, mortalityRates.getStreet1());
-      }
-      if (mortalityRates.getStreet2() == null) {
-        insertStmt.setNull(7, Types.VARCHAR);
-      } else {
-        insertStmt.setString(7, mortalityRates.getStreet2());
-      }
-      if (mortalityRates.getCity() == null) {
-        insertStmt.setNull(8, Types.VARCHAR);
-      } else {
-        insertStmt.setString(8, mortalityRates.getCity());
-      }
-      if (mortalityRates.getState() == null) {
-        insertStmt.setNull(9, Types.CHAR);
-      } else {
-        insertStmt.setString(9, mortalityRates.getState());
-      }
-      if (mortalityRates.getZipCode() == null) {
-        insertStmt.setNull(10, Types.CHAR);
-      } else {
-        insertStmt.setString(10, mortalityRates.getZipCode());
-      }
-      if (mortalityRates.getCuisine() == null) {
-        // Wasn't sure how to represent ENUMs here as there is no Enum type in Types for SQL
-        insertStmt.setNull(11, Types.VARCHAR);
-      } else {
-        insertStmt.setString(11, mortalityRates.getCuisine().name());
-      }
-      if (mortalityRates.getCounty() == null) {
-        insertStmt.setNull(12, Types.INTEGER);
-      } else {
-        insertStmt.setInt(12, mortalityRates.getCounty().getCompanyKey());
-      }
-      insertStmt.executeUpdate();
+      if (covidByRace.getNegative() == null) {
+          insertStmt.setNull(1, Types.VARCHAR);
+        } else {
+          insertStmt.setInt(1, covidByRace.getNegative());
+        }
+      if (covidByRace.getDeath() == null) {
+          insertStmt.setNull(1, Types.VARCHAR);
+        } else {
+          insertStmt.setInt(1, covidByRace.getDeath());
+        }
 
       resultKey = insertStmt.getGeneratedKeys();
-      int restaurantKey = -1;
+      int covidByRaceKey = -1;
       if (resultKey.next()) {
-        restaurantKey = resultKey.getInt(1);
+        covidByRaceKey = resultKey.getInt(1);
       } else {
         throw new SQLException("Unable to retrieve auto-generated key.");
       }
-      mortalityRates.setRestaurantKey(restaurantKey);
-      return mortalityRates;
+      covidByRace.setCovidByRaceKey(covidByRaceKey);
+      return covidByRace;
     } catch (SQLException e) {
       e.printStackTrace();
       throw e;
@@ -126,43 +86,31 @@ public class CovidByRaceDao {
   }
 
 
-  public MortalityRates getRestaurantById(int restaurantId) throws SQLException {
-    String selectRestaurant =
-            "SELECT RestaurantKey,Name,Description,Menu,ListedHours,IsActive,Street1,"
-                    + "Street2,City,State,ZipCode,Cuisine,CompanyKey "
-                    + "FROM Restaurant "
-                    + "WHERE RestaurantKey=?;";
+  public List<CovidByRace> getCovidByRaceByState(State state) throws SQLException {
+	List<CovidByRace> covidByRaces = new ArrayList<CovidByRace>();
+    String selectCovidByRace =
+            "SELECT CovidByRaceKey,StateFKey,Race,Postive,Negative,Death,Date FROM CovidByRace " +
+                    "WHERE StateFKey=?;";
     Connection connection = null;
     PreparedStatement selectStmt = null;
     ResultSet results = null;
     try {
       connection = connectionManager.getConnection();
-      selectStmt = connection.prepareStatement(selectRestaurant);
-      selectStmt.setInt(1, restaurantId);
+      selectStmt = connection.prepareStatement(selectCovidByRace);
+      selectStmt.setInt(1, state.getStateKey());
       results = selectStmt.executeQuery();
 
-      CountyDao countyDao = CountyDao.getInstance();
-
-      if (results.next()) {
-        int resultsRestaurantKey = results.getInt("RestaurantKey");
-        String name = results.getString("Name");
-        String description = results.getString("Description");
-        String menu = results.getString("Menu");
-        String listedHours = results.getString("ListedHours");
-        Boolean isActive = results.getBoolean("IsActive");
-        String street1 = results.getString("Street1");
-        String street2 = results.getString("Street2");
-        String city = results.getString("City");
-        String state = results.getString("State");
-        String zipCode = results.getString("ZipCode");
-        String cuisine = results.getString("Cuisine");
-        int companyKey = results.getInt("CompanyKey");
-
-        County county = countyDao.getCompanyById(companyKey);
-        MortalityRates mortalityRates = new MortalityRates(resultsRestaurantKey, name, description,menu,
-                listedHours,isActive,street1,street2,city,state,zipCode,
-                MortalityRates.CuisineType.valueOf(cuisine), county);
-        return mortalityRates;
+      while(results.next()) {
+        int covidByRaceKey = results.getInt("CovidByRaceKey");
+        Date resyear = results.getDate("Date");
+        String resrace = results.getString("Race");
+        int respos = results.getInt("Positive");
+        int resneg = results.getInt("Negative");
+        int resdeath = results.getInt("Death");
+        
+        CovidByRace.RaceType resultrace = CovidByRace.RaceType.valueOf(resrace);
+        CovidByRace covidByRace = new CovidByRace(covidByRaceKey, state, resultrace, respos, resneg, resdeath, resyear);
+        covidByRaces.add(covidByRace);
       }
     } catch (SQLException e) {
       e.printStackTrace();
@@ -178,136 +126,47 @@ public class CovidByRaceDao {
         results.close();
       }
     }
-    return null;
+    return covidByRaces;
   }
 
-  // Note: I think there was a typo in the spec for the method argument 'cusine' instead of
-  // cuisine, so I made it cuisine instead.
-  public List<MortalityRates> getRestaurantsByCuisine(MortalityRates.CuisineType cuisine) throws SQLException{
-    List<MortalityRates> restaurants = new ArrayList<MortalityRates>();
-    String selectRestaurant =
-            "SELECT RestaurantKey,Name,Description,Menu,ListedHours,IsActive,Street1,"
-                    + "Street2,City,State,ZipCode,Cuisine,CompanyKey "
-                    + "FROM Restaurant "
-                    + "WHERE Cuisine=?;";
-    Connection connection = null;
-    PreparedStatement selectStmt = null;
-    ResultSet results = null;
-    try {
-      connection = connectionManager.getConnection();
-      selectStmt = connection.prepareStatement(selectRestaurant);
-      selectStmt.setString(1, cuisine.toString());
-      results = selectStmt.executeQuery();
-
-      CountyDao countyDao = CountyDao.getInstance();
-      while(results.next()) {
-        int restaurantKey = results.getInt("RestaurantKey");
-        String name = results.getString("Name");
-        String description = results.getString("Description");
-        String menu = results.getString("Menu");
-        String listedHours = results.getString("ListedHours");
-        Boolean isActive = results.getBoolean("IsActive");
-        String street1 = results.getString("Street1");
-        String street2 = results.getString("Street2");
-        String city = results.getString("City");
-        String state = results.getString("State");
-        String zipCode = results.getString("ZipCode");
-        String cuisineResult = results.getString("Cuisine");
-        int companyKey = results.getInt("CompanyKey");
-
-        County county = countyDao.getCompanyById(companyKey);
-        MortalityRates mortalityRates = new MortalityRates(restaurantKey, name, description,menu,
-                listedHours,isActive,street1,street2,city,state,zipCode,
-                MortalityRates.CuisineType.valueOf(cuisineResult), county);
-        restaurants.add(mortalityRates);
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-      throw e;
-    } finally {
-      if(connection != null) {
-        connection.close();
-      }
-      if(selectStmt != null) {
-        selectStmt.close();
-      }
-      if(results != null) {
-        results.close();
-      }
-    }
-    return restaurants;
-  }
-
-  public List<MortalityRates> getRestaurantsByCompanyName(String companyName) throws SQLException{
-    List<MortalityRates> restaurants = new ArrayList<MortalityRates>();
-    String selectRestaurant =
-            "SELECT RestaurantKey,Name,Description,Menu,ListedHours,IsActive,Street1,"
-                    + "Street2,City,State,ZipCode,Cuisine,CompanyKey "
-                    + "FROM Restaurant "
-                    + "WHERE CompanyKey=?;";
-    Connection connection = null;
-    PreparedStatement selectStmt = null;
-    ResultSet results = null;
-    try {
-      connection = connectionManager.getConnection();
-      selectStmt = connection.prepareStatement(selectRestaurant);
-
-      CountyDao countyDao = CountyDao.getInstance();
-      County county = countyDao.getCompanyByCompanyName(companyName);
-
-      selectStmt.setInt(1, county.getCompanyKey());
-      results = selectStmt.executeQuery();
+  public CovidByRace updateCovidByRace(CovidByRace covidByRace, State state) throws SQLException {
+		String updateCovidByRace = "UPDATE CovidByRace SET StateFKey=? WHERE CovidByRaceKey=?;";
+		Connection connection = null;
+		PreparedStatement updateStmt = null;
+		try {
+			connection = connectionManager.getConnection();
+			updateStmt = connection.prepareStatement(updateCovidByRace);
+			updateStmt.setInt(1, state.getStateKey());
+			updateStmt.setInt(2, covidByRace.getCovidByRaceKey());
+			updateStmt.executeUpdate();
+			covidByRace.setState(state);
+			return covidByRace;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(updateStmt != null) {
+				updateStmt.close();
+			}
+		}
+	}
 
 
-      while(results.next()) {
-        int restaurantKey = results.getInt("RestaurantKey");
-        String name = results.getString("Name");
-        String description = results.getString("Description");
-        String menu = results.getString("Menu");
-        String listedHours = results.getString("ListedHours");
-        Boolean isActive = results.getBoolean("IsActive");
-        String street1 = results.getString("Street1");
-        String street2 = results.getString("Street2");
-        String city = results.getString("City");
-        String state = results.getString("State");
-        String zipCode = results.getString("ZipCode");
-        String cuisine = results.getString("Cuisine");
 
-        // Re-using company that we found above to avoid duplicating work
-        MortalityRates mortalityRates = new MortalityRates(restaurantKey, name, description,menu,
-                listedHours,isActive,street1,street2,city,state,zipCode,
-                MortalityRates.CuisineType.valueOf(cuisine), county);
-        restaurants.add(mortalityRates);
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-      throw e;
-    } finally {
-      if(connection != null) {
-        connection.close();
-      }
-      if(selectStmt != null) {
-        selectStmt.close();
-      }
-      if(results != null) {
-        results.close();
-      }
-    }
-    return restaurants;
-  }
-
-
-  public MortalityRates delete(MortalityRates mortalityRates) throws SQLException {
-    String deleteRestaurant = "DELETE FROM Restaurant WHERE RestaurantKey=?;";
+  public CovidByRace delete(CovidByRace covidByRace) throws SQLException {
+    String deleteCovidByRace = "DELETE FROM CovidByRace WHERE CovidByRaceKey=?;";
     Connection connection = null;
     PreparedStatement deleteStmt = null;
     try {
       connection = connectionManager.getConnection();
-      deleteStmt = connection.prepareStatement(deleteRestaurant);
-      deleteStmt.setInt(1, mortalityRates.getRestaurantKey());
+      deleteStmt = connection.prepareStatement(deleteCovidByRace);
+      deleteStmt.setInt(1, covidByRace.getCovidByRaceKey());
       deleteStmt.executeUpdate();
 
-      // Return null so the caller can no longer operate on the Restaurant instance.
+      // Return null so the caller can no longer operate on the Company instance.
       return null;
     } catch (SQLException e) {
       e.printStackTrace();
@@ -321,5 +180,5 @@ public class CovidByRaceDao {
       }
     }
   }
-}
 
+}
