3221789e5d97ad8858c4e3ad61834094ae01060f
rajes95 <sakhamuru.r@husky.neu.edu>
2020-07-17 13:11:23 -0700

    PM05 Q2 graphs and data are uploaded

--- PM05.sql
+++ PM05.sql
@@ -37,10 +37,10 @@ LINES TERMINATED BY '\n' IGNORE 1 ROWS
 (@state,@marchRate,@aprilRate,@monthlyChange)
 SET `State`=@state,`March2020`=@marchRate,`April2020`=@aprilRate,`OverTheMonthChange`=@monthlyChange;
 
-# March vs April per state Unemployment and newCasesPer100000
+# March vs April per state Unemployment and newCasesPer10000
 SELECT PostalCode, `month`, 
 	(CASE WHEN month = 3 THEN March2020 ELSE April2020 END) AS UnemploymentRate,
-    ((SUM(NumOfNewCases)/StatePop) * 100000) AS newCasesPer100000 FROM 
+    ((SUM(NumOfNewCases)/StatePop) * 10000) AS newCasesPer10000 FROM 
     (SELECT MONTH(CovidDate) AS `month`, StateKey, PostalCode, StatePop, March2020, April2020, OverTheMonthChange, NumOfNewCases FROM 
     (SELECT * FROM 
 	(SELECT PostalCode, StatePop, StateFKey FROM 
@@ -61,12 +61,12 @@ ORDER BY `month`, StateKey) marchAprilCovidUnemployment
 GROUP BY `month`, StateKey
 ORDER BY PostalCode, `month`;
 
-# increase in unemployment vs increase in CovidCasesPer100000 per state.
-SELECT PostalCode, OverTheMonthChange AS IncreaseInUnemploymentMarchToApril, (SUM(newAprilCasesPer100000)-SUM(newMarchCasesPer100000)) AS increaseInCasesPer100000  FROM
-	(SELECT PostalCode, OverTheMonthChange,	(CASE WHEN month = 3 THEN newCasesPer100000 ELSE 0 END) AS newMarchCasesPer100000,
-    (CASE WHEN month = 4 THEN newCasesPer100000 ELSE 0 END) AS newAprilCasesPer100000 FROM (SELECT PostalCode, `month`,
+# increase in unemployment vs increase in CovidCasesPer10000 per state.
+SELECT PostalCode, OverTheMonthChange AS IncreaseInUnemploymentMarchToApril, (SUM(newAprilCasesPer10000)-SUM(newMarchCasesPer10000)) AS increaseInCasesPer10000  FROM
+	(SELECT PostalCode, OverTheMonthChange,	(CASE WHEN month = 3 THEN newCasesPer10000 ELSE 0 END) AS newMarchCasesPer10000,
+    (CASE WHEN month = 4 THEN newCasesPer10000 ELSE 0 END) AS newAprilCasesPer10000 FROM (SELECT PostalCode, `month`,
     OverTheMonthChange, 
-    ((SUM(NumOfNewCases)/StatePop) * 100000) AS newCasesPer100000 FROM 
+    ((SUM(NumOfNewCases)/StatePop) * 10000) AS newCasesPer10000 FROM 
     (SELECT MONTH(CovidDate) AS `month`, StateKey, PostalCode, StatePop, March2020, April2020, OverTheMonthChange, NumOfNewCases FROM 
     (SELECT * FROM 
 	(SELECT PostalCode, StatePop, StateFKey FROM 
@@ -87,7 +87,7 @@ ORDER BY `month`, StateKey) marchAprilCovidUnemployment
 GROUP BY `month`, StateKey
 ORDER BY PostalCode, `month`) tbl0)tbl1
 GROUP BY PostalCode
-ORDER BY IncreaseInUnemploymentMarchToApril DESC;
+ORDER BY PostalCode;
 
 
 -- DROP TABLE IF EXISTS `CovidifyUSA`.`NasdaqStage`;
