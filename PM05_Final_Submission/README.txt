b14f7c460a4aa27e8c3d55ea82dbe320d1f60811
rajes95 <sakhamuru.r@husky.neu.edu>
2020-07-24 16:40:45 -0700

    PM05 Final Submission added

---
+++ PM05_Final_Submission/README.txt
@@ -0,0 +1,7 @@
+PM05
+Group Name: REAL Team Six
+Members:
+Rajesh Sakhamuru
+Elise Jortberg
+Ari Fleischer
+Lily Bessette
---
+++ PM05_Final_Submission/Source_1_NASDAQ/PM05_Q1.sql
@@ -0,0 +1,29 @@
+USE `CovidifyUSA`;
+DROP TABLE IF EXISTS `CovidifyUSA`.`NasdaqStage`;
+
+#1
+CREATE TABLE IF NOT EXISTS `CovidifyUSA`.`NasdaqStage` (
+  `Date` DATE,
+  `AdjClose` DECIMAL(10,2)
+  )
+ENGINE = INNODB;
+LOAD DATA INFILE './nasdaq_apr2019-apr2020.csv' 
+INTO TABLE `CovidifyUSA`.`NasdaqStage` FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n' IGNORE 1 ROWS
+(@date,@high,@low,@open,@close,@volume,@adjClose)
+SET `Date`=@date,`AdjClose`=@adjClose;
+
+# Nasdaq Adjusted Close vs New Covid Cases vs New Covid Deaths per day
+SELECT `Date`, AdjClose, NumOfNewCases, NumOfNewDeaths FROM NasdaqStage INNER JOIN
+(SELECT `Covid1`.`Date` AS CovidDate, SUM(`Covid1`.`CovidCases` - IFNULL((`Covid2`.`CovidCases`), 0)) AS `NumOfNewCases`, SUM(`Covid1`.`CovidDeaths` - IFNULL((`Covid2`.`CovidDeaths`), 0)) AS `NumOfNewDeaths`
+FROM `CovidByDate` AS `Covid1` LEFT JOIN `CovidByDate` AS `Covid2` 
+		ON DATEDIFF(`Covid1`.`Date`, `Covid2`.`Date`) = 1 AND `Covid1`.`CountyFKey` = `Covid2`.`CountyFKey`
+        JOIN `County` ON `Covid1`.`CountyFKey` = `County`.`CountyKey`
+GROUP BY `Covid1`.`Date`
+ORDER BY `Covid1`.`Date`) newCases ON newCases.CovidDate=NasdaqStage.`Date`
+INTO OUTFILE './Q1_NasdaqVSCovidData.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'; 
+
+DROP TABLE IF EXISTS `CovidifyUSA`.`NasdaqStage`;
