20277e0907775cb15db99365f0be24d98f11eddf
elisejortberg <jortberg.e@husky.neu.edu>
2020-07-29 23:22:59 -0400

    Add files via upload

---
+++ Eclipse/src/covidify/servlet/FindCovidByRace.java
@@ -0,0 +1,111 @@
+package covidify.servlet;
+
+import covidify.dal.*;
+import covidify.model.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * FindCovidByRace is an entry point into the application.
+ * <p>
+ * Note the logic for doGet() and doPost() are almost identical. However, there is a difference:
+ * doGet() handles the http GET request. This method is called when you put in the /findcountys URL
+ * in the browser. doPost() handles the http POST request. This method is called after you click the
+ * submit button.
+ * <p>
+ * To run: 1. Run the SQL script to recreate your database schema: http://goo.gl/86a11H. 2. Insert
+ * test data. You can do this by running blog.tools.Inserter (right click, Run As > JavaApplication.
+ * Notice that this is similar to Runner.java in our JDBC example. 3. Run the Tomcat server at
+ * localhost. 4. Point your browser to http://localhost:8080/BlogApplication/findcountys.
+ */
+
+@WebServlet("/findcovidbyrace")
+public class FindCovidByRace extends HttpServlet {
+  protected CovidByRaceDao covidByRaceDao;
+  protected StateDao stateDao;
+
+
+  @Override
+  public void init() throws ServletException {
+    covidByRaceDao = CovidByRaceDao.getInstance();
+    stateDao = StateDao.getInstance();
+  }
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+          throws ServletException, IOException {
+    // Map for storing messages.
+    Map<String, String> messages = new HashMap<String, String>();
+    req.setAttribute("messages", messages);
+    State state = null;
+    List<CovidByRace> covidbyrace = null;
+    // Retrieve and validate name.
+    String statename = req.getParameter("statename");
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    if (statename == null || statename.trim().isEmpty()) {
+      messages.put("success", "Please enter a valid County and State name pair and valid Date.");
+    } else {
+      try {
+        state = stateDao.getStateByName(statename);
+        covidbyrace = covidByRaceDao.getCovidByRaceByState(state);
+      } catch (SQLException e) {
+        e.printStackTrace();
+        throw new IOException(e);
+      }
+      messages.put("success", "Displaying results for " + statename);
+      // Save the previous search term, so it can be used as the default
+      // in the input box when rendering FindCovidByDate.jsp.
+      messages.put("previousStateName", statename);
+    }
+    req.setAttribute("covidbyrace", covidbyrace);
+
+    req.getRequestDispatcher("/FindCovidByRace.jsp").forward(req, resp);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp)
+          throws ServletException, IOException {
+    // Map for storing messages.
+    Map<String, String> messages = new HashMap<String, String>();
+    req.setAttribute("messages", messages);
+
+    State state = null;
+    List<CovidByRace> covidbyrace = null;
+    // Retrieve and validate name.
+    // countyname and statename is retrieved from the form POST submission. By default, it
+    // is populated by the URL query string (in FindCovidByDate.jsp).
+    String statename = req.getParameter("statename");
+    if (statename == null || statename.trim().isEmpty()) {
+      messages.put("success", "Please enter a valid State name");
+    } else {
+      try {
+        state = stateDao.getStateByName(statename);
+        covidbyrace = covidByRaceDao.getCovidByRaceByState(state);
+      } catch (SQLException e) {
+        e.printStackTrace();
+        throw new IOException(e);
+      }
+      messages.put("success", "Displaying results for "  + statename);
+    }
+    req.setAttribute("covidbyrace", covidbyrace);
+
+    req.getRequestDispatcher("/FindCovidByRace.jsp").forward(req, resp);
+  }
+}
+
